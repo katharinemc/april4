e868b8d2009d98273e9df7b97a9ffc1b45d9a243
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-04 14:37:42 -0600

    Add database and define FindOne method

--- bonusdatabasesearch.js
+++ bonusdatabasesearch.js
@@ -1,20 +1,35 @@
 'use strict'; 
 
-const HEROES = [
-  { id: 1, name: 'Captain America', squad: 'Avengers' },
-  { id: 2, name: 'Iron Man', squad: 'Avengers' },
-  { id: 3, name: 'Spiderman', squad: 'Avengers' },
-  { id: 4, name: 'Superman', squad: 'Justice League' },
-  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
-  { id: 6, name: 'Aquaman', squad: 'Justice League' },
-  { id: 7, name: 'Hulk', squad: 'Avengers' },
-];
-
-// function helperFunction(query) {
-//   return queryKeys = Object.keys(query);
-// }
-
-
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function findOne(query) {
+    let result = this.store.heroes.find(character => {for (let key in query) {
+      if (query[key] !== character[key]) {
+        return false;
+      }
+    }
+      /// Go over "for... in" in greater depth;
+    return true;}   
+    );
+  
+    if (result === undefined) {
+      return null;
+    }
+    else {
+      return result;
+    }
+  },
+};
 
 function findOne(arr, query) {
   let result = arr.find(character => {for (let key in query) {
@@ -22,7 +37,7 @@ function findOne(arr, query) {
       return false;
     }
   }
-/// Go over "for... in" in greater depth;
+    /// Go over "for... in" in greater depth;
   return true;}   
   );
 
@@ -35,5 +50,4 @@ function findOne(arr, query) {
 }
 
 
-console.log(findOne(HEROES, { id: 1 }));
-console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }));
+console.log(Database.findOne({ squad: 'Avengers' }));
